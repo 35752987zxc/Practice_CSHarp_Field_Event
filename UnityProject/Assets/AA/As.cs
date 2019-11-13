a00f5adca71e9cc18789d721edc9fde11146870b
35752987zxc <57181192+35752987zxc@users.noreply.github.com>
2019-11-13 20:35:14 +0800

    000

---
+++ UnityProject/Assets/AA/As.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class As : MonoBehaviour
+{  //小雞
+    [Header("雞"), Range(0, 999)]
+    public int ru = 999;
+    [Header("速度"),Range(0.1f,10f)]
+    public float rul = 10f;
+    [Tooltip("111")]
+    public string k = "111";
+    public bool a = true;
+
+
+  
+    }
